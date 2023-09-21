d5e98f5acf439639d82931df45ffa8e6b4454180
hubcio <h.gruszecki@gmail.com>
2023-09-21 06:42:52 +0200

    Fix nested env var config overriding (#65)
    This fixes #61.

--- server/src/components/config_provider.rs
+++ server/src/components/config_provider.rs
@@ -1,10 +1,15 @@
+use async_trait::async_trait;
+use figment::{
+    providers::{Format, Json, Toml},
+    value::{Dict, Map as FigmentMap, Value as FigmentValue},
+    Error, Figment, Metadata, Profile, Provider,
+};
+use std::env;
+use toml::{map::Map, Value as TomlValue};
+use tracing::info;
+
 use crate::configs::server::ServerConfig;
 use crate::server_error::ServerError;
-use async_trait::async_trait;
-use figment::providers::{Env, Format, Json, Toml};
-use figment::Figment;
-use std::env;
-use tracing::{error, info};
 
 const DEFAULT_CONFIG_PROVIDER: &str = "file";
 const DEFAULT_CONFIG_PATH: &str = "configs/server.toml";
@@ -17,6 +22,135 @@ pub trait ConfigProvider {
 #[derive(Debug)]
 pub struct FileConfigProvider {
     path: String,
+}
+
+pub struct CustomEnvProvider {
+    prefix: String,
+}
+
+impl FileConfigProvider {
+    pub fn new(path: String) -> Self {
+        Self { path }
+    }
+}
+
+impl CustomEnvProvider {
+    pub fn new(prefix: &str) -> Self {
+        Self {
+            prefix: prefix.to_string(),
+        }
+    }
+
+    fn walk_toml_table_to_dict(prefix: &str, table: Map<String, TomlValue>, dict: &mut Dict) {
+        for (key, value) in table {
+            let new_prefix = if prefix.is_empty() {
+                key.clone()
+            } else {
+                format!("{}.{}", prefix, key)
+            };
+            match value {
+                TomlValue::Table(inner_table) => {
+                    let mut nested_dict = Dict::new();
+                    Self::walk_toml_table_to_dict(&new_prefix, inner_table, &mut nested_dict);
+                    dict.insert(key, FigmentValue::from(nested_dict));
+                }
+                _ => {
+                    dict.insert(key, Self::toml_to_figment_value(&value));
+                }
+            }
+        }
+    }
+
+    fn find_and_insert_nested_key(dict: &mut Dict, keys: Vec<String>, value: FigmentValue) {
+        let mut current_dict = dict;
+        for i in 0..keys.len() {
+            let combined_keys = keys[i..].join("_");
+            if let std::collections::btree_map::Entry::Occupied(mut e) =
+                current_dict.entry(combined_keys)
+            {
+                e.insert(value);
+                return;
+            }
+
+            let key = &keys[i];
+            if let Some(FigmentValue::Dict(_, ref mut next_dict)) = current_dict.get_mut(key) {
+                current_dict = next_dict;
+            } else {
+                return;
+            }
+        }
+    }
+
+    fn toml_to_figment_value(toml_value: &TomlValue) -> FigmentValue {
+        match toml_value {
+            TomlValue::String(s) => FigmentValue::from(s.clone()),
+            TomlValue::Integer(i) => FigmentValue::from(*i),
+            TomlValue::Float(f) => FigmentValue::from(*f),
+            TomlValue::Boolean(b) => FigmentValue::from(*b),
+            TomlValue::Array(arr) => {
+                let vec: Vec<FigmentValue> = arr.iter().map(Self::toml_to_figment_value).collect();
+                FigmentValue::from(vec)
+            }
+            TomlValue::Table(tbl) => {
+                let mut dict = figment::value::Dict::new();
+                for (key, value) in tbl.iter() {
+                    dict.insert(key.clone(), Self::toml_to_figment_value(value));
+                }
+                FigmentValue::from(dict)
+            }
+            TomlValue::Datetime(_) => todo!("not implemented yet!"),
+        }
+    }
+
+    fn try_parse_value(value: String) -> FigmentValue {
+        if value == "true" {
+            return FigmentValue::from(true);
+        }
+        if value == "false" {
+            return FigmentValue::from(false);
+        }
+        if let Ok(int_val) = value.parse::<i64>() {
+            return FigmentValue::from(int_val);
+        }
+        if let Ok(float_val) = value.parse::<f64>() {
+            return FigmentValue::from(float_val);
+        }
+        FigmentValue::from(value)
+    }
+}
+impl Provider for CustomEnvProvider {
+    fn metadata(&self) -> Metadata {
+        Metadata::named("iggy-server config")
+    }
+
+    fn data(&self) -> Result<FigmentMap<Profile, Dict>, Error> {
+        let toml_value: TomlValue =
+            toml::from_str(include_str!("../../../configs/server.toml")).unwrap();
+        let mut toml_as_dict = Dict::new();
+        if let TomlValue::Table(table) = toml_value {
+            Self::walk_toml_table_to_dict("", table, &mut toml_as_dict);
+        }
+        for (key, value) in env::vars() {
+            let env_key = key.to_uppercase();
+            if !env_key.starts_with(self.prefix.as_str()) {
+                continue;
+            }
+            let keys: Vec<String> = env_key[self.prefix.len()..]
+                .split('_')
+                .map(|k| k.to_lowercase())
+                .collect();
+            let env_var_value = Self::try_parse_value(value);
+            info!(
+                "{} value changed to: {:?} from environment variable",
+                env_key, env_var_value
+            );
+            Self::find_and_insert_nested_key(&mut toml_as_dict, keys, env_var_value);
+        }
+        let mut data = FigmentMap::new();
+        data.insert(Profile::default(), toml_as_dict);
+
+        Ok(data)
+    }
 }
 
 pub fn resolve(config_provider_type: &str) -> Result<Box<dyn ConfigProvider>, ServerError> {
@@ -32,12 +166,6 @@ pub fn resolve(config_provider_type: &str) -> Result<Box<dyn ConfigProvider>, Se
     }
 }
 
-impl FileConfigProvider {
-    pub fn new(path: String) -> FileConfigProvider {
-        FileConfigProvider { path }
-    }
-}
-
 #[async_trait]
 impl ConfigProvider for FileConfigProvider {
     async fn load_config(&self) -> Result<ServerConfig, ServerError> {
@@ -48,20 +176,20 @@ impl ConfigProvider for FileConfigProvider {
             "json" => config_builder.merge(Json::file(&self.path)),
             "toml" => config_builder.merge(Toml::file(&self.path)),
             _ => {
-                error!("Cannot load configuration: invalid file extension, only .json and .toml are supported.");
-                return Err(ServerError::CannotLoadConfiguration);
+                return Err(ServerError::CannotLoadConfiguration("Cannot load configuration: invalid file extension, only .json and .toml are supported.".to_owned()));
             }
         };
-
-        let config: Result<ServerConfig, figment::Error> = config_builder
-            .merge(Env::prefixed("IGGY_").split("_"))
-            .extract();
-        if config.is_err() {
-            return Err(ServerError::CannotLoadConfiguration);
+        let custom_env_provider = CustomEnvProvider::new("IGGY_");
+        let config_builder = config_builder.merge(custom_env_provider);
+        match config_builder.extract::<ServerConfig>() {
+            Ok(config) => {
+                info!("Config loaded from path: '{}'", self.path);
+                Ok(config)
+            }
+            Err(figment_error) => Err(ServerError::CannotLoadConfiguration(format!(
+                "Failed to load configuration: {}",
+                figment_error
+            ))),
         }
-
-        let config = config.unwrap();
-        info!("Config loaded from path: '{}'", self.path);
-        Ok(config)
     }
 }
--- server/tests/config_provider_tests/mod.rs
+++ server/tests/config_provider_tests/mod.rs
@@ -1,4 +1,5 @@
 use server::components::config_provider::{ConfigProvider, FileConfigProvider};
+use std::env;
 use std::path::{Path, PathBuf};
 
 fn file_exists(file_path: &str) -> bool {
@@ -10,13 +11,10 @@ fn get_root_path() -> PathBuf {
     PathBuf::from(manifest_dir)
 }
 
-async fn scenario(extension: &str) {
+async fn scenario_parsing_from_file(extension: &str) {
     let mut config_path = get_root_path().join("../configs/server");
     assert!(config_path.set_extension(extension), "Cannot set extension");
-    let config_path = config_path
-        .to_str()
-        .expect("Failed to convert config file to String")
-        .to_owned();
+    let config_path = config_path.as_path().display().to_string();
     let config_provider = FileConfigProvider::new(config_path.clone());
     assert!(
         file_exists(&config_path),
@@ -32,10 +30,39 @@ async fn scenario(extension: &str) {
 
 #[tokio::test]
 async fn validate_server_config_toml_from_repository() {
-    scenario("toml").await;
+    scenario_parsing_from_file("toml").await;
 }
 
 #[tokio::test]
 async fn validate_server_config_json_from_repository() {
-    scenario("json").await;
+    scenario_parsing_from_file("json").await;
+}
+
+#[tokio::test]
+async fn validate_custom_env_provider() {
+    env::set_var("IGGY_SYSTEM_DATABASE_PATH", "awesome_database_path");
+    env::set_var("IGGY_QUIC_DATAGRAM_SEND_BUFFER_SIZE", "1337");
+    env::set_var("IGGY_QUIC_CERTIFICATE_SELF_SIGNED", "false");
+    env::set_var("IGGY_HTTP_ENABLED", "false");
+    env::set_var("IGGY_SYSTEM_PARTITION_MESSAGES_REQUIRED_TO_SAVE", "42");
+
+    let config_path = get_root_path().join("../configs/server.toml");
+
+    let file_config_provider = FileConfigProvider::new(config_path.as_path().display().to_string());
+    let config = file_config_provider
+        .load_config()
+        .await
+        .expect("Failed to load default server.toml config");
+
+    assert_eq!(config.system.database.path, "awesome_database_path");
+    assert_eq!(config.quic.datagram_send_buffer_size, 1337);
+    assert!(!config.quic.certificate.self_signed);
+    assert!(!config.http.enabled);
+    assert_eq!(config.system.partition.messages_required_to_save, 42);
+
+    env::remove_var("IGGY_SYSTEM_DATABASE_PATH");
+    env::remove_var("IGGY_QUIC_DATAGRAM_SEND_BUFFER_SIZE");
+    env::remove_var("IGGY_QUIC_CERTIFICATE_SELF_SIGNED");
+    env::remove_var("IGGY_HTTP_ENABLED");
+    env::remove_var("IGGY_SYSTEM_PARTITION_MESSAGES_REQUIRED_TO_SAVE");
 }

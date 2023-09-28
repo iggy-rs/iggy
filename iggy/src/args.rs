7073005c502886204afb9348772035c51c45d3c7
Bartosz Cieśla <bartosz.ciesla@gmail.com>
2023-09-28 19:35:58 +0200

    Add two default features to iggy username-in-args and password-in-args
    Features control presence of username and password members in Args
    struct used for iggy SDK parameters parsing. Both features are enabled
    by default in iggy crate. Both are disabled in iggy-cmd crate in order
    to be able to introduce different handling of both parameters in
    iggy command line tool.

--- iggy/src/args.rs
+++ iggy/src/args.rs
@@ -6,9 +6,11 @@ pub struct Args {
     #[arg(long, default_value = "tcp")]
     pub transport: String,
 
+    #[cfg(feature = "username-in-args")]
     #[arg(long, default_value = "iggy")]
     pub username: String,
 
+    #[cfg(feature = "password-in-args")]
     #[arg(long, default_value = "iggy")]
     pub password: String,
 

19b6c498b4afcee367d5e27ccfd5d4dff2b4b966
hubcio <h.gruszecki@gmail.com>
2023-10-05 17:18:40 +0200

    Use jemalloc as standard allocator

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 mod args;
 mod binary;
 mod components;

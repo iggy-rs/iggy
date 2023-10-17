dc1f598259324184a555e9cdef465f36246cf410
seroze <serozekim@gmail.com>
2023-10-17 18:22:07 +0530

    fix cargo fmt

--- server/src/streaming/systems/info.rs
+++ server/src/streaming/systems/info.rs
@@ -167,12 +167,12 @@ impl Display for SemanticVersion {
     }
 }
 
-mod tests{
+mod tests {
 
     use super::*;
 
     #[test]
-    fn should_load_the_expected_version_from_package_definition(){
+    fn should_load_the_expected_version_from_package_definition() {
         const CARGO_TOML_VERSION: &str = env!("CARGO_PKG_VERSION");
         assert_eq!(VERSION, CARGO_TOML_VERSION);
     }

2d8cb570abfc045b905406033d204a20a291749a
Bartosz Cieśla <31999821+BartoszCiesla@users.noreply.github.com>
2023-11-13 13:00:52 +0100

    Fix coverage CI flow and disable test on arm and aarch64 (#297)

--- integration/tests/cmd/personal_access_token/mod.rs
+++ integration/tests/cmd/personal_access_token/mod.rs
@@ -2,4 +2,6 @@ mod test_pat_create_command;
 mod test_pat_delete_command;
 mod test_pat_help_command;
 mod test_pat_list_command;
+// Disable tests due to missing keyring on arm and aarch64 until #294 is implemented
+#[cfg(not(any(target_arch = "aarch64", target_arch = "arm")))]
 mod test_pat_login_options;

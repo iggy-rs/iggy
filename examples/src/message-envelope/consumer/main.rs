11b18f8deeeb866d11626e33bf45db994a364990
David Yu <davidfrankyu@gmail.com>
2023-12-31 12:47:45 -0500

    Added unit tests to examples/src/message-envelope/consumer/main.rs

--- examples/src/message-envelope/consumer/main.rs
+++ examples/src/message-envelope/consumer/main.rs
@@ -63,3 +63,182 @@ fn handle_message(message: &Message) -> Result<(), Box<dyn Error>> {
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::Bytes;
+    use examples::shared::utils;
+    use iggy::models::messages::MessageState;
+    use serde_json::json;
+    use tracing_test::traced_test;
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_succeed_for_valid_order_created_type() {
+        let payload_json = json!({
+            "message_type": "order_created",
+            "payload": OrderCreated {
+                order_id: 1,
+                currency_pair: "EUR/USD".to_string(),
+                price: 3.45,
+                quantity: 1.0,
+                side: "buy".to_string(),
+                timestamp: utils::timestamp(),
+            }
+            .to_json()
+        })
+        .to_string();
+        let message = Message::empty(
+            0,
+            MessageState::Available,
+            0,
+            Bytes::from(payload_json),
+            0,
+            None,
+        );
+        let result = handle_message(&message);
+        assert!(result.is_ok());
+        assert!(logs_contain(
+            "Handling message type: order_created at offset: 0..."
+        ));
+        assert!(logs_contain("OrderCreated"));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 2));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_succeed_for_valid_order_confirmed_type() {
+        let payload_json = json!({
+            "message_type": "order_confirmed",
+            "payload": OrderConfirmed {
+                order_id: 1,
+                price: 3.45,
+                timestamp: utils::timestamp(),
+            }
+            .to_json()
+        })
+        .to_string();
+        let message = Message::empty(
+            0,
+            MessageState::Available,
+            0,
+            Bytes::from(payload_json),
+            0,
+            None,
+        );
+        let result = handle_message(&message);
+        assert!(result.is_ok());
+        assert!(logs_contain(
+            "Handling message type: order_confirmed at offset: 0..."
+        ));
+        assert!(logs_contain("OrderConfirmed"));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 2));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_succeed_for_valid_order_rejected_type() {
+        let payload_json = json!({
+            "message_type": "order_rejected",
+            "payload": OrderRejected {
+                order_id: 1,
+                timestamp: utils::timestamp(),
+                reason: "cancelled_by_user".to_string(),
+            }
+            .to_json()
+        })
+        .to_string();
+        let message = Message::empty(
+            0,
+            MessageState::Available,
+            0,
+            Bytes::from(payload_json),
+            0,
+            None,
+        );
+        let result = handle_message(&message);
+        assert!(result.is_ok());
+        assert!(logs_contain(
+            "Handling message type: order_rejected at offset: 0..."
+        ));
+        assert!(logs_contain("OrderRejected"));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 2));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_warn_for_invalid_order_type() {
+        let payload_json = json!({
+            "message_type": "order_unknown",
+            "payload": "invalid_payload"
+        })
+        .to_string();
+        let message = Message::empty(
+            0,
+            MessageState::Available,
+            0,
+            Bytes::from(payload_json),
+            0,
+            None,
+        );
+        let result = handle_message(&message);
+        assert!(result.is_ok());
+        assert!(logs_contain(
+            "Handling message type: order_unknown at offset: 0..."
+        ));
+        assert!(logs_contain("Received unknown message type: order_unknown"));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 1));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 1));
+    }
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_report_error_for_valid_order_type_with_invalid_payload() {
+        let payload_json = json!({
+            "message_type": "order_created",
+            "payload": "invalid_payload"
+        })
+        .to_string();
+        let message = Message::empty(
+            0,
+            MessageState::Available,
+            0,
+            Bytes::from(payload_json),
+            0,
+            None,
+        );
+        let result = handle_message(&message);
+        assert!(result.is_err());
+        assert!(logs_contain(
+            "Handling message type: order_created at offset: 0..."
+        ));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 1));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_report_error_for_missing_order_type() {
+        let payload_json = json!({
+            "payload": "invalid_payload"
+        })
+        .to_string();
+        let message = Message::empty(
+            0,
+            MessageState::Available,
+            0,
+            Bytes::from(payload_json),
+            0,
+            None,
+        );
+        let result = handle_message(&message);
+        assert!(result.is_err());
+        assert!(!logs_contain("Handling message type:"));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 0));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+}

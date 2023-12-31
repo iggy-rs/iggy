0b662efdd86909fd314d42e109ade7ca4083d36e
David Yu <davidfrankyu@gmail.com>
2023-12-31 13:06:39 -0500

    Added unit tests to examples/src/getting-started/producer/main.rs

--- examples/src/getting-started/producer/main.rs
+++ examples/src/getting-started/producer/main.rs
@@ -32,7 +32,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     produce_messages(&client).await
 }
 
-async fn init_system(client: &IggyClient) {
+async fn init_system(client: &dyn Client) {
     match client
         .create_stream(&CreateStream {
             stream_id: STREAM_ID,
@@ -96,5 +96,87 @@ async fn produce_messages(client: &dyn Client) -> Result<(), Box<dyn Error>> {
         sent_batches += 1;
         info!("Sent {messages_per_batch} message(s).");
         sleep(interval).await;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use examples::shared::mock_client::*;
+    use examples::shared::utils;
+    use iggy::error::Error;
+    use iggy::topics::create_topic::CreateTopic;
+    use mockall::predicate;
+    use tracing_test::traced_test;
+
+    static CREATE_STREAM_SUCCESS_OUTPUT: &str = "Stream was created.";
+    static CREATE_TOPIC_SUCCESS_OUTPUT: &str = "Topic was created.";
+    static CREATE_STREAM_ERROR_OUTPUT: &str =
+        "Stream already exists and will not be created again.";
+    static CREATE_TOPIC_ERROR_OUTPUT: &str = "Topic already exists and will not be created again.";
+
+    fn setup_mock_client(existing_stream: bool, existing_topic: bool) -> MockIggyClient {
+        let mut mock_client = MockIggyClient::new();
+        let expected_create_stream = CreateStream {
+            stream_id: STREAM_ID,
+            name: "sample-stream".to_string(),
+        };
+        let expected_create_topic: CreateTopic = CreateTopic {
+            stream_id: Identifier::numeric(STREAM_ID).unwrap(),
+            topic_id: TOPIC_ID,
+            partitions_count: 1,
+            name: "sample-topic".to_string(),
+            message_expiry: None,
+        };
+        mock_client
+            .expect_create_stream()
+            .with(predicate::eq(expected_create_stream)) // Use predicate::eq() to compare references
+            .times(1)
+            .returning(move |_| {
+                if !existing_stream {
+                    Ok(())
+                } else {
+                    Err(Error::Error)
+                }
+            });
+        mock_client
+            .expect_create_topic()
+            .with(predicate::eq(expected_create_topic)) // Use predicate::eq() to compare references
+            .times(1)
+            .returning(move |_| {
+                if !existing_topic {
+                    Ok(())
+                } else {
+                    Err(Error::Error)
+                }
+            });
+        return mock_client;
+    }
+
+    #[tokio::test]
+    #[traced_test]
+    async fn should_init_system_with_no_existing_stream_or_topic() {
+        let mock_client = setup_mock_client(false, false);
+        init_system(&mock_client).await;
+        assert!(logs_contain(CREATE_STREAM_SUCCESS_OUTPUT));
+        assert!(logs_contain(CREATE_TOPIC_SUCCESS_OUTPUT));
+        assert!(!logs_contain(CREATE_STREAM_ERROR_OUTPUT));
+        assert!(!logs_contain(CREATE_TOPIC_ERROR_OUTPUT));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 2));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+
+    #[tokio::test]
+    #[traced_test]
+    async fn should_init_system_with_existing_stream_and_topic() {
+        let mock_client = setup_mock_client(true, true);
+        init_system(&mock_client).await;
+
+        assert!(logs_contain(CREATE_STREAM_ERROR_OUTPUT));
+        assert!(logs_contain(CREATE_TOPIC_ERROR_OUTPUT));
+        assert!(!logs_contain(CREATE_STREAM_SUCCESS_OUTPUT));
+        assert!(!logs_contain(CREATE_TOPIC_SUCCESS_OUTPUT));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 0));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 2));
     }
 }

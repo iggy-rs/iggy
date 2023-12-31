0015078e076168b99a0b9b4973ea99805d293772
David Yu <davidfrankyu@gmail.com>
2023-12-31 12:44:09 -0500

    Added unit tests to examples/src/getting-started/consumer/main.rs

--- examples/src/getting-started/consumer/main.rs
+++ examples/src/getting-started/consumer/main.rs
@@ -84,3 +84,45 @@ fn handle_message(message: &Message) -> Result<(), Box<dyn Error>> {
     );
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bytes::Bytes;
+    use examples::shared::utils;
+    use iggy::models::messages::MessageState;
+    use tracing_test::traced_test;
+
+    static HANDLE_MESSAGE_INFO_OUTPUT: &str =
+        "Handling message at offset: 0, payload: message-1...";
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_succeed_with_valid_payload() {
+        let message = Message::empty(
+            0,
+            MessageState::Available,
+            0,
+            Bytes::from("message-1"),
+            0,
+            None,
+        );
+        let result = handle_message(&message);
+        assert!(result.is_ok());
+        assert!(logs_contain(HANDLE_MESSAGE_INFO_OUTPUT));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 1));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+
+    #[test]
+    #[traced_test]
+    fn handle_message_should_report_error_with_invalid_payload() {
+        let invalid_utf8_bytes = Bytes::from_static(b"\xC3\x28"); // Invalid UTF-8 sequence
+        let message = Message::empty(0, MessageState::Available, 0, invalid_utf8_bytes, 0, None);
+        let result = handle_message(&message);
+        assert!(result.is_err());
+        assert!(!logs_contain(HANDLE_MESSAGE_INFO_OUTPUT));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "INFO", 0));
+        logs_assert(|lines: &[&str]| utils::matching_log_entry_counts(lines, "WARN", 0));
+    }
+}

846186d70cff4940f6418dc3a2c07fbdcf4b5a02
David Yu <davidfrankyu@gmail.com>
2023-12-31 11:53:20 -0500

    Initial commit: add unit test helper function matching_log_entry_counts to examples shared::utils

--- examples/src/shared/utils.rs
+++ examples/src/shared/utils.rs
@@ -6,3 +6,17 @@ pub fn timestamp() -> u64 {
         .unwrap()
         .as_micros() as u64
 }
+
+pub fn matching_log_entry_counts(
+    lines: &[&str],
+    log_level: &str,
+    expected_count: usize,
+) -> Result<(), String> {
+    match lines.iter().filter(|line| line.contains(log_level)).count() {
+        n if n == expected_count => Ok(()),
+        x => Err(format!(
+            "Expected {} matching {} log entry, but found {}",
+            expected_count, log_level, x
+        )),
+    }
+}

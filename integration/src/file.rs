2cd8fcc155ce7bee7d70e6aa3f64a60b3d2f2961
dfy313 <148644444+dfy313@users.noreply.github.com>
2024-01-09 04:28:24 -0500

    Test `examples` in CI (#496)
    Co-authored-by: Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
    Co-authored-by: Hubert Gruszecki <h.gruszecki@gmail.com>

--- integration/src/file.rs
+++ integration/src/file.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 pub fn get_root_path() -> PathBuf {
@@ -5,6 +6,9 @@ pub fn get_root_path() -> PathBuf {
     PathBuf::from(manifest_dir)
 }
 
-pub fn file_exists(file_path: &str) -> bool {
+pub fn file_exists<S>(file_path: &S) -> bool
+where
+    S: AsRef<OsStr> + ?Sized,
+{
     Path::new(file_path).is_file()
 }
---
+++ integration/tests/examples/test_message_envelope.rs
@@ -0,0 +1,57 @@
+use super::{parse_sent_message, verify_stdout_contains_expected_logs};
+use crate::examples::{IggyExampleTest, IggyExampleTestCase};
+use serial_test::parallel;
+
+struct TestMessageEnvelope<'a> {
+    expected_producer_output: Vec<&'a str>,
+    expected_consumer_output: Vec<&'a str>,
+}
+
+impl<'a> IggyExampleTestCase for TestMessageEnvelope<'a> {
+    fn verify_log_output(&self, producer_stdout: &str, consumer_stdout: &str) {
+        verify_stdout_contains_expected_logs(
+            producer_stdout,
+            consumer_stdout,
+            &self.expected_producer_output,
+            &self.expected_consumer_output,
+        );
+    }
+
+    fn verify_message_output(&self, producer_stdout: &str, consumer_stdout: &str) {
+        let producer_captured_message = parse_sent_message(producer_stdout);
+        for line in producer_captured_message.lines() {
+            let trimmed_line = line.trim();
+            assert!(
+                consumer_stdout.contains(trimmed_line),
+                "Consumer output does not contain expected line: '{}'",
+                trimmed_line
+            );
+        }
+    }
+}
+
+#[tokio::test]
+#[parallel]
+async fn should_successfully_execute() {
+    let mut iggy_example_test = IggyExampleTest::new("message-envelope", None);
+    iggy_example_test.setup(false).await;
+
+    iggy_example_test
+        .execute_test(TestMessageEnvelope {
+            expected_producer_output: vec![
+                "Message envelope producer has started, selected transport: tcp",
+                "Received an invalid response with status: 1009 (stream_id_not_found).",
+                "Stream does not exist, creating...",
+                "Messages will be sent to stream: 9999, topic: 1, partition: 1 with interval 1000 ms.",
+            ],
+            expected_consumer_output: vec![
+                "Message envelope consumer has started, selected transport: tcp",
+                "Validating if stream: 9999 exists..",
+                "Stream: 9999 was found.",
+                "Validating if topic: 1 exists..",
+                "Topic: 1 was found.",
+                "Messages will be polled by consumer: 1 from stream: 9999, topic: 1, partition: 1 with interval 1000 ms.",
+            ],
+        })
+        .await;
+}

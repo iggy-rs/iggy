91483fdb62c0179d1de9bb60d4f1b682de1a31f8
yaohuiming <yaohuiming789@gmail.com>
2024-01-13 01:39:20 +0800

    Refactor QUIC stream handling

--- iggy/src/messages/mod.rs
+++ iggy/src/messages/mod.rs
@@ -2,4 +2,4 @@ pub mod poll_messages;
 pub mod send_messages;
 
 const MAX_HEADERS_SIZE: u32 = 100 * 1000;
-const MAX_PAYLOAD_SIZE: u32 = 10 * 1000 * 1000;
+pub const MAX_PAYLOAD_SIZE: u32 = 10 * 1000 * 1000;

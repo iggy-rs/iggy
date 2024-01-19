a9b98c48b05efce4c7202a197f3a6ff76cecbd91
gibbz00 <gabrielhansson00@gmail.com>
2024-01-19 17:35:15 +0100

    Monomorphized `AccessTokenClient` impl for `BinaryClient` in SDK.

--- sdk/src/binary/personal_access_tokens.rs
+++ sdk/src/binary/personal_access_tokens.rs
@@ -1,6 +1,7 @@
 use crate::binary::binary_client::{BinaryClient, ClientState};
 use crate::binary::{fail_if_not_authenticated, mapper};
 use crate::bytes_serializable::BytesSerializable;
+use crate::client::PersonalAccessTokenClient;
 use crate::command::*;
 use crate::error::Error;
 use crate::models::identity_info::IdentityInfo;
@@ -10,46 +11,48 @@ use crate::personal_access_tokens::delete_personal_access_token::DeletePersonalA
 use crate::personal_access_tokens::get_personal_access_tokens::GetPersonalAccessTokens;
 use crate::personal_access_tokens::login_with_personal_access_token::LoginWithPersonalAccessToken;
 
-pub async fn get_personal_access_tokens(
-    client: &dyn BinaryClient,
-    command: &GetPersonalAccessTokens,
-) -> Result<Vec<PersonalAccessTokenInfo>, Error> {
-    fail_if_not_authenticated(client).await?;
-    let response = client
-        .send_with_response(GET_PERSONAL_ACCESS_TOKENS_CODE, &command.as_bytes())
-        .await?;
-    mapper::map_personal_access_tokens(&response)
-}
+#[async_trait::async_trait]
+impl<B: BinaryClient> PersonalAccessTokenClient for B {
+    async fn get_personal_access_tokens(
+        &self,
+        command: &GetPersonalAccessTokens,
+    ) -> Result<Vec<PersonalAccessTokenInfo>, Error> {
+        fail_if_not_authenticated(self).await?;
+        let response = self
+            .send_with_response(GET_PERSONAL_ACCESS_TOKENS_CODE, &command.as_bytes())
+            .await?;
+        mapper::map_personal_access_tokens(&response)
+    }
 
-pub async fn create_personal_access_token(
-    client: &dyn BinaryClient,
-    command: &CreatePersonalAccessToken,
-) -> Result<RawPersonalAccessToken, Error> {
-    fail_if_not_authenticated(client).await?;
-    let response = client
-        .send_with_response(CREATE_PERSONAL_ACCESS_TOKEN_CODE, &command.as_bytes())
-        .await?;
-    mapper::map_raw_pat(&response)
-}
+    async fn create_personal_access_token(
+        &self,
+        command: &CreatePersonalAccessToken,
+    ) -> Result<RawPersonalAccessToken, Error> {
+        fail_if_not_authenticated(self).await?;
+        let response = self
+            .send_with_response(CREATE_PERSONAL_ACCESS_TOKEN_CODE, &command.as_bytes())
+            .await?;
+        mapper::map_raw_pat(&response)
+    }
 
-pub async fn delete_personal_access_token(
-    client: &dyn BinaryClient,
-    command: &DeletePersonalAccessToken,
-) -> Result<(), Error> {
-    fail_if_not_authenticated(client).await?;
-    client
-        .send_with_response(DELETE_PERSONAL_ACCESS_TOKEN_CODE, &command.as_bytes())
-        .await?;
-    Ok(())
-}
+    async fn delete_personal_access_token(
+        &self,
+        command: &DeletePersonalAccessToken,
+    ) -> Result<(), Error> {
+        fail_if_not_authenticated(self).await?;
+        self.send_with_response(DELETE_PERSONAL_ACCESS_TOKEN_CODE, &command.as_bytes())
+            .await?;
+        Ok(())
+    }
 
-pub async fn login_with_personal_access_token(
-    client: &dyn BinaryClient,
-    command: &LoginWithPersonalAccessToken,
-) -> Result<IdentityInfo, Error> {
-    let response = client
-        .send_with_response(LOGIN_WITH_PERSONAL_ACCESS_TOKEN_CODE, &command.as_bytes())
-        .await?;
-    client.set_state(ClientState::Authenticated).await;
-    mapper::map_identity_info(&response)
+    async fn login_with_personal_access_token(
+        &self,
+        command: &LoginWithPersonalAccessToken,
+    ) -> Result<IdentityInfo, Error> {
+        let response = self
+            .send_with_response(LOGIN_WITH_PERSONAL_ACCESS_TOKEN_CODE, &command.as_bytes())
+            .await?;
+        self.set_state(ClientState::Authenticated).await;
+        mapper::map_identity_info(&response)
+    }
 }
--- sdk/src/quic/mod.rs
+++ sdk/src/quic/mod.rs
@@ -1,5 +1,4 @@
 pub mod client;
 pub mod config;
-pub mod personal_access_tokens;
 pub mod system;
 pub mod users;

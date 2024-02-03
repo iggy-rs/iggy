2eca8dacf18f589937af0c0e407f781bf7769b11
Hubert Gruszecki <h.gruszecki@gmail.com>
2024-02-03 16:03:44 +0100

    Add support for building with `tokio-console` feature
    - Include `tokio-console` feature in `server/Cargo.toml`
    - Add `console-subscriber` dependency conditionally
    - Implement `tokio_console` module for logging
    - Update `justfile` with `build-tokio-console` command
    - Rename `logging` module to `log` and adjust imports

---
+++ server/src/log/tokio_console.rs
@@ -0,0 +1,34 @@
+use crate::configs::system::LoggingConfig;
+use crate::server_error::ServerError;
+use tracing_subscriber::prelude::*;
+
+pub struct Logging {}
+
+impl Logging {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn early_init(&mut self) {
+        let console_layer = console_subscriber::spawn();
+
+        let subscriber = tracing_subscriber::registry().with(console_layer);
+
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("Setting global default subscriber failed");
+    }
+
+    pub fn late_init(
+        &mut self,
+        _base_directory: String,
+        _config: &LoggingConfig,
+    ) -> Result<(), ServerError> {
+        Ok(())
+    }
+}
+
+impl Default for Logging {
+    fn default() -> Self {
+        Self::new()
+    }
+}

8cec798f1e2ea1531c3efa2b9b472eab49bcfd23
Bartosz Cieśla <31999821+BartoszCiesla@users.noreply.github.com>
2024-03-27 16:13:43 +0100

    Add message size validation scenario for TCP server (#847)

--- server/src/tcp/connection_handler.rs
+++ server/src/tcp/connection_handler.rs
@@ -7,6 +7,7 @@ use crate::streaming::systems::system::SharedSystem;
 use bytes::{BufMut, BytesMut};
 use iggy::bytes_serializable::BytesSerializable;
 use iggy::command::Command;
+use iggy::error::IggyError;
 use std::io::ErrorKind;
 use std::net::SocketAddr;
 use tracing::{debug, error, info};
@@ -23,11 +24,19 @@ pub(crate) async fn handle_connection(
     let session = Session::from_client_id(client_id, address);
     let mut initial_buffer = [0u8; INITIAL_BYTES_LENGTH];
     loop {
-        let read_length = sender.read(&mut initial_buffer).await?;
+        let read_length = match sender.read(&mut initial_buffer).await {
+            Ok(read_length) => read_length,
+            Err(error) => {
+                sender.send_error_response(error).await?;
+                continue;
+            }
+        };
+
         if read_length != INITIAL_BYTES_LENGTH {
-            return Err(ServerError::CommandLengthError(format!(
+            sender.send_error_response(IggyError::CommandLengthError(format!(
                 "Unable to read the TCP request length, expected: {INITIAL_BYTES_LENGTH} bytes, received: {read_length} bytes."
-            )));
+            ))).await?;
+            continue;
         }
 
         let length = u32::from_le_bytes(initial_buffer);
@@ -35,7 +44,13 @@ pub(crate) async fn handle_connection(
         let mut command_buffer = BytesMut::with_capacity(length as usize);
         command_buffer.put_bytes(0, length as usize);
         sender.read(&mut command_buffer).await?;
-        let command = Command::from_bytes(command_buffer.freeze())?;
+        let command = match Command::from_bytes(command_buffer.freeze()) {
+            Ok(command) => command,
+            Err(error) => {
+                sender.send_error_response(error).await?;
+                continue;
+            }
+        };
         debug!("Received a TCP command: {command}, payload size: {length}");
         command::handle(&command, sender, &session, system.clone()).await?;
         debug!("Sent a TCP response.");
@@ -61,7 +76,9 @@ pub(crate) fn handle_error(error: ServerError) {
                 error!("Connection has failed: {error}");
             }
         },
-        ServerError::SdkError(_) => {}
+        ServerError::SdkError(sdk_error) => {
+            error!("Failure in internal SDK call: {sdk_error}");
+        }
         _ => {
             error!("Connection has failed: {error}");
         }

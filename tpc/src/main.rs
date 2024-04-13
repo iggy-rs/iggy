15ed0dc7ae9a148e27a9c2a934f35b293cbdd234
spetz <piotr.gankiewicz@gmail.com>
2024-04-13 10:24:34 +0200

    Update monoio and other crates, initial main file

--- tpc/src/main.rs
+++ tpc/src/main.rs
@@ -1,7 +1,12 @@
 use figlet_rs::FIGfont;
 use monoio::utils::CtrlC;
 use std::error::Error;
+use clap::Parser;
 use tracing::info;
+use server::args::Args;
+use server::configs::config_provider;
+use server::configs::server::ServerConfig;
+use server::streaming::systems::system::{SharedSystem, System};
 
 #[monoio::main(timer_enabled = true)]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -9,6 +14,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let standard_font = FIGfont::standard().unwrap();
     let figure = standard_font.convert("Iggy TPC");
     println!("{}", figure.unwrap());
+
+
+    let args = Args::parse();
+
+    let config_provider = config_provider::resolve(&args.config_provider)?;
+    let config = ServerConfig::load(config_provider.as_ref()).await?;
+
+    let system = SharedSystem::new(System::new(
+        config.system.clone(),
+        None,
+        config.personal_access_token,
+    ));
+
+    system.write().get_stats_bypass_auth().await?;
+    system.write().init().await?;
+
     info!("Press CTRL+C shutdown Iggy TPC...");
     CtrlC::new().unwrap().await;
     info!("Iggy TPC has shutdown successfully.");

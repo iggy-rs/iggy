47f65de05eea2dc56ee6edc3d35832c7ca406768
Grzegorz Koszyk <112548209+numinnex@users.noreply.github.com>
2024-06-17 19:40:11 +0200

    Rollback changes to address binding in integration tests (#1001)

--- server/src/tcp/tcp_server.rs
+++ server/src/tcp/tcp_server.rs
@@ -1,21 +1,22 @@
 use crate::configs::tcp::TcpConfig;
 use crate::streaming::systems::system::SharedSystem;
 use crate::tcp::{tcp_listener, tcp_tls_listener};
+use std::net::SocketAddr;
 use tracing::info;
 
 /// Starts the TCP server.
 /// Returns the address the server is listening on.
-pub async fn start(config: TcpConfig, system: SharedSystem) {
+pub async fn start(config: TcpConfig, system: SharedSystem) -> SocketAddr {
     let server_name = if config.tls.enabled {
         "Iggy TCP TLS"
     } else {
         "Iggy TCP"
     };
     info!("Initializing {server_name} server...");
-    if config.tls.enabled {
-        tcp_tls_listener::start(&config.address, config.tls, system).await
-    } else {
-        tcp_listener::start(&config.address, system).await
+    let addr = match config.tls.enabled {
+        true => tcp_tls_listener::start(&config.address, config.tls, system).await,
+        false => tcp_listener::start(&config.address, system).await,
     };
-    info!("{server_name} server has started on: {:?}", &config.address);
+    info!("{server_name} server has started on: {:?}", addr);
+    addr
 }

6036a76d81423cfe275add244f60eb909f35e567
spetz <piotr.gankiewicz@gmail.com>
2024-06-23 18:58:56 +0200

    Add required checks when loading the state

--- server/src/state/system.rs
+++ server/src/state/system.rs
@@ -29,7 +29,7 @@ use iggy::utils::timestamp::IggyTimestamp;
 use iggy::utils::topic_size::MaxTopicSize;
 use std::collections::HashMap;
 use std::fmt::Display;
-use tracing::{debug, warn};
+use tracing::{debug, error};
 
 #[derive(Debug)]
 pub struct SystemState {
@@ -121,7 +121,9 @@ impl SystemState {
                 UPDATE_STREAM_CODE => {
                     let command = UpdateStream::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     stream.name = command.name;
                 }
                 DELETE_STREAM_CODE => {
@@ -132,7 +134,9 @@ impl SystemState {
                 CREATE_TOPIC_CODE => {
                     let command = CreateTopic::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     let topic_id = command.topic_id.unwrap_or_else(|| {
                         stream.current_topic_id += 1;
                         stream.current_topic_id
@@ -168,9 +172,14 @@ impl SystemState {
                 UPDATE_TOPIC_CODE => {
                     let command = UpdateTopic::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     let topic_id = find_topic_id(&stream.topics, &command.topic_id);
-                    let topic = stream.topics.get_mut(&topic_id).unwrap();
+                    let topic = stream
+                        .topics
+                        .get_mut(&topic_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Topic: {topic_id} not found")));
                     topic.name = command.name;
                     topic.compression_algorithm = command.compression_algorithm;
                     topic.message_expiry = command.message_expiry;
@@ -180,17 +189,33 @@ impl SystemState {
                 DELETE_TOPIC_CODE => {
                     let command = DeleteTopic::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     let topic_id = find_topic_id(&stream.topics, &command.topic_id);
                     stream.topics.remove(&topic_id);
                 }
                 CREATE_PARTITIONS_CODE => {
                     let command = CreatePartitions::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     let topic_id = find_topic_id(&stream.topics, &command.topic_id);
-                    let topic = stream.topics.get_mut(&topic_id).unwrap();
-                    let last_partition_id = topic.partitions.values().map(|p| p.id).max().unwrap();
+                    let topic = stream
+                        .topics
+                        .get_mut(&topic_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Topic: {topic_id} not found")));
+                    let last_partition_id = if topic.partitions.is_empty() {
+                        0
+                    } else {
+                        topic
+                            .partitions
+                            .values()
+                            .map(|p| p.id)
+                            .max()
+                            .unwrap_or_else(|| panic!("No partition found"))
+                    };
                     for i in 1..=command.partitions_count {
                         topic.partitions.insert(
                             last_partition_id + i,
@@ -204,10 +229,24 @@ impl SystemState {
                 DELETE_PARTITIONS_CODE => {
                     let command = DeletePartitions::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     let topic_id = find_topic_id(&stream.topics, &command.topic_id);
-                    let topic = stream.topics.get_mut(&topic_id).unwrap();
-                    let last_partition_id = topic.partitions.values().map(|p| p.id).max().unwrap();
+                    let topic = stream
+                        .topics
+                        .get_mut(&topic_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Topic: {topic_id} not found")));
+                    if topic.partitions.is_empty() {
+                        continue;
+                    }
+
+                    let last_partition_id = topic
+                        .partitions
+                        .values()
+                        .map(|p| p.id)
+                        .max()
+                        .unwrap_or_else(|| panic!("No partition found"));
                     for i in 0..command.partitions_count {
                         topic.partitions.remove(&(last_partition_id - i));
                     }
@@ -215,9 +254,14 @@ impl SystemState {
                 CREATE_CONSUMER_GROUP_CODE => {
                     let command = CreateConsumerGroup::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     let topic_id = find_topic_id(&stream.topics, &command.topic_id);
-                    let topic = stream.topics.get_mut(&topic_id).unwrap();
+                    let topic = stream
+                        .topics
+                        .get_mut(&topic_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Topic: {topic_id} not found")));
                     let consumer_group_id = command.group_id.unwrap_or_else(|| {
                         topic.current_consumer_group_id += 1;
                         topic.current_consumer_group_id
@@ -233,9 +277,14 @@ impl SystemState {
                 DELETE_CONSUMER_GROUP_CODE => {
                     let command = DeleteConsumerGroup::from_bytes(entry.payload)?;
                     let stream_id = find_stream_id(&streams, &command.stream_id);
-                    let stream = streams.get_mut(&stream_id).unwrap();
+                    let stream = streams
+                        .get_mut(&stream_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Stream: {stream_id} not found")));
                     let topic_id = find_topic_id(&stream.topics, &command.topic_id);
-                    let topic = stream.topics.get_mut(&topic_id).unwrap();
+                    let topic = stream
+                        .topics
+                        .get_mut(&topic_id)
+                        .unwrap_or_else(|| panic!("{}", format!("Topic: {topic_id} not found")));
                     let consumer_group_id =
                         find_consumer_group_id(&topic.consumer_groups, &command.group_id);
                     topic.consumer_groups.remove(&consumer_group_id);
@@ -256,7 +305,9 @@ impl SystemState {
                 UPDATE_USER_CODE => {
                     let command = UpdateUser::from_bytes(entry.payload)?;
                     let user_id = find_user_id(&users, &command.user_id);
-                    let user = users.get_mut(&user_id).unwrap();
+                    let user = users
+                        .get_mut(&user_id)
+                        .unwrap_or_else(|| panic!("{}", format!("User: {user_id} not found")));
                     if let Some(username) = &command.username {
                         user.username.clone_from(username);
                     }
@@ -272,20 +323,26 @@ impl SystemState {
                 CHANGE_PASSWORD_CODE => {
                     let command = ChangePassword::from_bytes(entry.payload)?;
                     let user_id = find_user_id(&users, &command.user_id);
-                    let user = users.get_mut(&user_id).unwrap();
+                    let user = users
+                        .get_mut(&user_id)
+                        .unwrap_or_else(|| panic!("{}", format!("User: {user_id} not found")));
                     user.password_hash = command.new_password // This is already hashed
                 }
                 UPDATE_PERMISSIONS_CODE => {
                     let command = UpdatePermissions::from_bytes(entry.payload)?;
                     let user_id = find_user_id(&users, &command.user_id);
-                    let user = users.get_mut(&user_id).unwrap();
+                    let user = users
+                        .get_mut(&user_id)
+                        .unwrap_or_else(|| panic!("{}", format!("User: {user_id} not found")));
                     user.permissions = command.permissions;
                 }
                 CREATE_PERSONAL_ACCESS_TOKEN_CODE => {
                     let command = CreatePersonalAccessTokenWithHash::from_bytes(entry.payload)?;
                     let token_hash = command.hash;
                     let user_id = find_user_id(&users, &entry.user_id.try_into()?);
-                    let user = users.get_mut(&user_id).unwrap();
+                    let user = users
+                        .get_mut(&user_id)
+                        .unwrap_or_else(|| panic!("{}", format!("User: {user_id} not found")));
                     let expiry_at = PersonalAccessToken::calculate_expiry_at(
                         entry.timestamp,
                         command.command.expiry,
@@ -309,11 +366,13 @@ impl SystemState {
                 DELETE_PERSONAL_ACCESS_TOKEN_CODE => {
                     let command = DeletePersonalAccessToken::from_bytes(entry.payload)?;
                     let user_id = find_user_id(&users, &entry.user_id.try_into()?);
-                    let user = users.get_mut(&user_id).unwrap();
+                    let user = users
+                        .get_mut(&user_id)
+                        .unwrap_or_else(|| panic!("{}", format!("User: {user_id} not found")));
                     user.personal_access_tokens.remove(&command.name);
                 }
                 code => {
-                    warn!("Unsupported state entry code: {code}");
+                    error!("Unsupported state entry code: {code}");
                 }
             }
         }
@@ -328,10 +387,17 @@ impl SystemState {
 
 fn find_stream_id(streams: &HashMap<u32, StreamState>, stream_id: &Identifier) -> u32 {
     match stream_id.kind {
-        IdKind::Numeric => stream_id.get_u32_value().unwrap(),
+        IdKind::Numeric => stream_id
+            .get_u32_value()
+            .unwrap_or_else(|_| panic!("{}", format!("Invalid stream ID: {stream_id}"))),
         IdKind::String => {
-            let name = stream_id.get_cow_str_value().unwrap();
-            let stream = streams.values().find(|s| s.name == name).unwrap();
+            let name = stream_id
+                .get_cow_str_value()
+                .unwrap_or_else(|_| panic!("{}", format!("Invalid stream name: {stream_id}")));
+            let stream = streams
+                .values()
+                .find(|s| s.name == name)
+                .unwrap_or_else(|| panic!("{}", format!("Stream: {name} not found")));
             stream.id
         }
     }
@@ -339,10 +405,17 @@ fn find_stream_id(streams: &HashMap<u32, StreamState>, stream_id: &Identifier) -
 
 fn find_topic_id(topics: &HashMap<u32, TopicState>, topic_id: &Identifier) -> u32 {
     match topic_id.kind {
-        IdKind::Numeric => topic_id.get_u32_value().unwrap(),
+        IdKind::Numeric => topic_id
+            .get_u32_value()
+            .unwrap_or_else(|_| panic!("{}", format!("Invalid topic ID: {topic_id}"))),
         IdKind::String => {
-            let name = topic_id.get_cow_str_value().unwrap();
-            let topic = topics.values().find(|s| s.name == name).unwrap();
+            let name = topic_id
+                .get_cow_str_value()
+                .unwrap_or_else(|_| panic!("{}", format!("Invalid topic name: {topic_id}")));
+            let topic = topics
+                .values()
+                .find(|s| s.name == name)
+                .unwrap_or_else(|| panic!("{}", format!("Topic: {name} not found")));
             topic.id
         }
     }
@@ -350,10 +423,17 @@ fn find_topic_id(topics: &HashMap<u32, TopicState>, topic_id: &Identifier) -> u3
 
 fn find_consumer_group_id(groups: &HashMap<u32, ConsumerGroupState>, group_id: &Identifier) -> u32 {
     match group_id.kind {
-        IdKind::Numeric => group_id.get_u32_value().unwrap(),
+        IdKind::Numeric => group_id
+            .get_u32_value()
+            .unwrap_or_else(|_| panic!("{}", format!("Invalid group ID: {group_id}"))),
         IdKind::String => {
-            let name = group_id.get_cow_str_value().unwrap();
-            let group = groups.values().find(|s| s.name == name).unwrap();
+            let name = group_id
+                .get_cow_str_value()
+                .unwrap_or_else(|_| panic!("{}", format!("Invalid group name: {group_id}")));
+            let group = groups
+                .values()
+                .find(|s| s.name == name)
+                .unwrap_or_else(|| panic!("{}", format!("Consumer group: {name} not found")));
             group.id
         }
     }
@@ -361,10 +441,17 @@ fn find_consumer_group_id(groups: &HashMap<u32, ConsumerGroupState>, group_id: &
 
 fn find_user_id(users: &HashMap<u32, UserState>, user_id: &Identifier) -> u32 {
     match user_id.kind {
-        IdKind::Numeric => user_id.get_u32_value().unwrap(),
+        IdKind::Numeric => user_id
+            .get_u32_value()
+            .unwrap_or_else(|_| panic!("{}", format!("Invalid user ID: {user_id}"))),
         IdKind::String => {
-            let username = user_id.get_cow_str_value().unwrap();
-            let user = users.values().find(|s| s.username == username).unwrap();
+            let username = user_id
+                .get_cow_str_value()
+                .unwrap_or_else(|_| panic!("{}", format!("Invalid username: {user_id}")));
+            let user = users
+                .values()
+                .find(|s| s.username == username)
+                .unwrap_or_else(|| panic!("{}", format!("User: {username} not found")));
             user.id
         }
     }

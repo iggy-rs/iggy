3e168441abe74b0df4f85b3973ac2e25ada27cb5
numinex <g.koszyk98@gmail.com>
2024-07-20 20:18:46 +0200

    Extend benchmark with latency percentiles

--- bench/src/producer.rs
+++ bench/src/producer.rs
@@ -1,5 +1,5 @@
 use crate::args::simple::BenchmarkKind;
-use crate::benchmark_result::BenchmarkResult;
+use crate::benchmark_result::{BenchmarkResult, LatencyPercentiles};
 use iggy::client::MessageClient;
 use iggy::clients::client::{IggyClient, IggyClientBackgroundConfig};
 use iggy::error::IggyError;
@@ -105,21 +105,42 @@ impl Producer {
             latencies.push(latency_end);
         }
         let end_timestamp = Instant::now();
+
+        latencies.sort();
+        let last_idx = latencies.len() - 1;
+        let p50 = latencies[last_idx / 2];
+        let p90 = latencies[last_idx * 9 / 10];
+        let p95 = latencies[last_idx * 95 / 100];
+        let p99 = latencies[last_idx * 99 / 100];
+        let p999 = latencies[last_idx * 999 / 1000];
+        let latency_percentiles = LatencyPercentiles {
+            p50,
+            p90,
+            p95,
+            p99,
+            p999,
+        };
+
         let duration = end_timestamp - start_timestamp;
         let average_latency: Duration = latencies.iter().sum::<Duration>() / latencies.len() as u32;
         let total_size_bytes = total_messages * self.message_size as u64;
         let average_throughput = total_size_bytes as f64 / duration.as_secs_f64() / 1e6;
 
         info!(
-        "Producer #{} → sent {} messages in {} batches of {} messages in {:.2} s, total size: {} bytes, average latency: {:.2} ms, average throughput: {:.2} MB/s",
+        "Producer #{} → sent {} messages in {} batches of {} messages in {:.2} s, total size: {} bytes, average throughput: {:.2} MB/s, p50 latency: {:.2} ms, p90 latency: {:.2} ms, p95 latency: {:.2} ms, p99 latency: {:.2} ms, p999 latency: {:.2} ms, average latency: {:.2} ms",
         self.producer_id,
         total_messages,
         self.message_batches,
         self.messages_per_batch,
         duration.as_secs_f64(),
         total_size_bytes,
-        average_latency.as_secs_f64() * 1000.0,
-        average_throughput
+        average_throughput,
+        p50.as_secs_f64() * 1000.0,
+        p90.as_secs_f64() * 1000.0,
+        p95.as_secs_f64() * 1000.0,
+        p99.as_secs_f64() * 1000.0,
+        p999.as_secs_f64() * 1000.0,
+        average_latency.as_secs_f64() * 1000.0
     );
 
         Ok(BenchmarkResult {
@@ -127,6 +148,7 @@ impl Producer {
             start_timestamp,
             end_timestamp,
             average_latency,
+            latency_percentiles,
             total_size_bytes,
             total_messages,
         })

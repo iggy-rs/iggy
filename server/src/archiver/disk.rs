7a2023010857f700e0845652b429b00168fd263a
bakytgerey_ashirbeko <bakytgerey_ashirbeko@ozon.ru>
2024-10-26 11:48:45 +0600

    add trace errors in server archiver

--- server/src/archiver/disk.rs
+++ server/src/archiver/disk.rs
@@ -2,6 +2,7 @@ use crate::archiver::Archiver;
 use crate::configs::server::DiskArchiverConfig;
 use crate::server_error::ArchiverError;
 use async_trait::async_trait;
+use error_set::ResultContext;
 use std::path::Path;
 use tokio::fs;
 use tracing::{debug, info};
@@ -22,7 +23,14 @@ impl Archiver for DiskArchiver {
     async fn init(&self) -> Result<(), ArchiverError> {
         if !Path::new(&self.config.path).exists() {
             info!("Creating disk archiver directory: {}", self.config.path);
-            fs::create_dir_all(&self.config.path).await?;
+            fs::create_dir_all(&self.config.path)
+                .await
+                .with_error(|err| {
+                    format!(
+                        "ARCHIVER - failed to create directory: {} with error: {err}",
+                        self.config.path
+                    )
+                })?;
         }
         Ok(())
     }
@@ -58,8 +66,14 @@ impl Archiver for DiskArchiver {
             let base_directory = base_directory.as_deref().unwrap_or_default();
             let destination = Path::new(&self.config.path).join(base_directory).join(file);
             let destination_path = destination.to_str().unwrap_or_default().to_owned();
-            fs::create_dir_all(destination.parent().unwrap()).await?;
-            fs::copy(source, destination).await?;
+            fs::create_dir_all(destination.parent().unwrap())
+                .await
+                .with_error(|err| {
+                    format!("ARCHIVER - failed to create file: {file} at path: {destination_path} with error: {err}",)
+                })?;
+            fs::copy(source, destination).await.with_error(|err| {
+                format!("ARCHIVER - failed to copy file: {file} to destination: {destination_path} with error: {err}")
+            })?;
             debug!("Archived file: {file} at: {destination_path}");
         }
 

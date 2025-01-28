fe4e38fd6c9f3f0bd79eeb45efc4529727d87b53
Hubert Gruszecki <h.gruszecki@gmail.com>
2025-01-28 21:39:39 +0100

    modify cargo.lock and comment code in consumer

--- bench/src/actors/consumer.rs
+++ bench/src/actors/consumer.rs
@@ -86,166 +86,166 @@ impl Consumer {
             None => IggyConsumer::new(self.consumer_id.try_into().unwrap()),
         };
 
-        let mut latencies: Vec<Duration> = Vec::with_capacity(message_batches as usize);
-        let mut total_user_data_bytes = IggyByteSize::default();
-        let mut total_bytes = IggyByteSize::default();
-        let mut current_iteration: u64 = 0;
-        let mut received_messages = 0;
-        let mut topic_not_found_counter = 0;
-        let mut strategy = PollingStrategy::offset(0);
+        todo!();
 
-        /*
-        if self.warmup_time.get_duration() != Duration::from_millis(0) {
-            if let Some(cg_id) = self.consumer_group_id {
-                info!(
-                    "Consumer #{}, part of consumer group #{}, → warming up for {}...",
-                    self.consumer_id, cg_id, self.warmup_time
-                );
-            } else {
-                info!(
-                    "Consumer #{} → warming up for {}...",
-                    self.consumer_id, self.warmup_time
-                );
-            }
-            let warmup_end = Instant::now() + self.warmup_time.get_duration();
-            while Instant::now() < warmup_end {
-                let offset = current_iteration * messages_per_batch as u64;
-                strategy.set_value(offset);
-                let polled_messages = client
-                    .poll_messages(
-                        &stream_id,
-                        &topic_id,
-                        partition_id,
-                        &consumer,
-                        &strategy,
-                        messages_per_batch,
-                        false,
-                    )
-                    .await?;
+        // let mut latencies: Vec<Duration> = Vec::with_capacity(message_batches as usize);
+        // let mut total_user_data_bytes = IggyByteSize::default();
+        // let mut total_bytes = IggyByteSize::default();
+        // let mut current_iteration: u64 = 0;
+        // let mut received_messages = 0;
+        // let mut topic_not_found_counter = 0;
+        // let mut strategy = PollingStrategy::offset(0);
 
-                if polled_messages.messages.is_empty() {
-                    warn!(
-                        "Consumer: {} - Messages are empty for offset: {}, retrying...",
-                        self.consumer_id, offset
-                    );
-                    continue;
-                }
-                current_iteration += 1;
-            }
-        }
+        // if self.warmup_time.get_duration() != Duration::from_millis(0) {
+        //     if let Some(cg_id) = self.consumer_group_id {
+        //         info!(
+        //             "Consumer #{}, part of consumer group #{}, → warming up for {}...",
+        //             self.consumer_id, cg_id, self.warmup_time
+        //         );
+        //     } else {
+        //         info!(
+        //             "Consumer #{} → warming up for {}...",
+        //             self.consumer_id, self.warmup_time
+        //         );
+        //     }
+        //     let warmup_end = Instant::now() + self.warmup_time.get_duration();
+        //     while Instant::now() < warmup_end {
+        //         let offset = current_iteration * messages_per_batch as u64;
+        //         strategy.set_value(offset);
+        //         let polled_messages = client
+        //             .poll_messages(
+        //                 &stream_id,
+        //                 &topic_id,
+        //                 partition_id,
+        //                 &consumer,
+        //                 &strategy,
+        //                 messages_per_batch,
+        //                 false,
+        //             )
+        //             .await?;
 
-        if let Some(cg_id) = self.consumer_group_id {
-            info!(
-            "Consumer #{}, part of consumer group #{} → polling {} messages in {} batches of {} messages...",
-            self.consumer_id, cg_id, total_messages, message_batches, messages_per_batch
-        );
-        } else {
-            info!(
-                "Consumer #{} → polling {} messages in {} batches of {} messages...",
-                self.consumer_id, total_messages, message_batches, messages_per_batch
-            );
-        }
+        //         // if polled_messages.messages.is_empty() {
+        //         //     warn!(
+        //         //         "Consumer: {} - Messages are empty for offset: {}, retrying...",
+        //         //         self.consumer_id, offset
+        //         //     );
+        //         //     continue;
+        //         // }
+        //         current_iteration += 1;
+        //     }
+        // }
 
-        current_iteration = 0;
-        let start_timestamp = Instant::now();
-        let mut records = Vec::with_capacity(message_batches as usize);
-        let mut batch_user_size_bytes = 0;
-        let mut batch_size_total_bytes = 0;
-        while received_messages < total_messages {
-            let offset = current_iteration * messages_per_batch as u64;
+        // if let Some(cg_id) = self.consumer_group_id {
+        //     info!(
+        //     "Consumer #{}, part of consumer group #{} → polling {} messages in {} batches of {} messages...",
+        //     self.consumer_id, cg_id, total_messages, message_batches, messages_per_batch
+        // );
+        // } else {
+        //     info!(
+        //         "Consumer #{} → polling {} messages in {} batches of {} messages...",
+        //         self.consumer_id, total_messages, message_batches, messages_per_batch
+        //     );
+        // }
 
-            let before_poll = Instant::now();
-            let polled_messages = client
-                .poll_messages(
-                    &stream_id,
-                    &topic_id,
-                    partition_id,
-                    &consumer,
-                    &PollingStrategy::offset(offset),
-                    messages_per_batch,
-                    false,
-                )
-                .await;
-            let latency = before_poll.elapsed();
+        // current_iteration = 0;
+        // let start_timestamp = Instant::now();
+        // let mut records = Vec::with_capacity(message_batches as usize);
+        // let mut batch_user_size_bytes = 0;
+        // let mut batch_size_total_bytes = 0;
+        // while received_messages < total_messages {
+        //     let offset = current_iteration * messages_per_batch as u64;
 
-            if let Err(e) = polled_messages {
-                if matches!(e, IggyError::TopicIdNotFound(_, _)) {
-                    topic_not_found_counter += 1;
-                    if topic_not_found_counter > 1000 {
-                        return Err(e);
-                    }
-                } else {
-                    return Err(e);
-                }
-                error!("Unexpected error: {:?}", e);
-                continue;
-            }
+        //     let before_poll = Instant::now();
+        //     let polled_messages = client
+        //         .poll_messages(
+        //             &stream_id,
+        //             &topic_id,
+        //             partition_id,
+        //             &consumer,
+        //             &PollingStrategy::offset(offset),
+        //             messages_per_batch,
+        //             false,
+        //         )
+        //         .await;
+        //     let latency = before_poll.elapsed();
 
-            let polled_messages = polled_messages.unwrap();
-            if polled_messages.messages.is_empty() {
-                warn!(
-                    "Consumer: {} - Messages are empty for offset: {}, retrying...",
-                    self.consumer_id, offset
-                );
-                continue;
-            }
+        //     if let Err(e) = polled_messages {
+        //         if matches!(e, IggyError::TopicIdNotFound(_, _)) {
+        //             topic_not_found_counter += 1;
+        //             if topic_not_found_counter > 1000 {
+        //                 return Err(e);
+        //             }
+        //         } else {
+        //             return Err(e);
+        //         }
+        //         error!("Unexpected error: {:?}", e);
+        //         continue;
+        //     }
 
-            if polled_messages.messages.len() != messages_per_batch as usize {
-                warn!(
-                    "Consumer #{} → expected {} messages, but got {} messages, retrying...",
-                    self.consumer_id,
-                    messages_per_batch,
-                    polled_messages.messages.len()
-                );
-                continue;
-            }
+        // let polled_messages = polled_messages.unwrap();
+        // if polled_messages.messages.is_empty() {
+        //     warn!(
+        //         "Consumer: {} - Messages are empty for offset: {}, retrying...",
+        //         self.consumer_id, offset
+        //     );
+        //     continue;
+        // }
 
-            latencies.push(latency);
+        // if polled_messages.messages.len() != messages_per_batch as usize {
+        //     warn!(
+        //         "Consumer #{} → expected {} messages, but got {} messages, retrying...",
+        //         self.consumer_id,
+        //         messages_per_batch,
+        //         polled_messages.messages.len()
+        //     );
+        //     continue;
+        // }
 
-            received_messages += polled_messages.messages.len() as u64;
+        // latencies.push(latency);
 
-            // We don't need to calculate the size whole batch every time by iterating over it - just always use the size of the first message
-            if batch_user_size_bytes == 0 || batch_size_total_bytes == 0 {
-                batch_user_size_bytes =
-                    polled_messages.messages[0].payload.len() as u64 * messages_per_batch as u64;
-                batch_size_total_bytes =
-                    polled_messages.messages[0].get_size_bytes().as_bytes_u64()
-                        * messages_per_batch as u64;
-            }
+        // received_messages += polled_messages.messages.len() as u64;
 
-            total_user_data_bytes += IggyByteSize::from(batch_user_size_bytes);
-            total_bytes += IggyByteSize::from(batch_size_total_bytes);
-            current_iteration += 1;
-            let message_batches = current_iteration;
-            records.push(BenchmarkRecord {
-                elapsed_time_us: start_timestamp.elapsed().as_micros() as u64,
-                latency_us: latency.as_micros() as u64,
-                messages: received_messages,
-                message_batches,
-                user_data_bytes: total_user_data_bytes.as_bytes_u64(),
-                total_bytes: total_bytes.as_bytes_u64(),
-            });
-        }
+        // // We don't need to calculate the size whole batch every time by iterating over it - just always use the size of the first message
+        // if batch_user_size_bytes == 0 || batch_size_total_bytes == 0 {
+        //     batch_user_size_bytes =
+        //         polled_messages.messages[0].payload.len() as u64 * messages_per_batch as u64;
+        //     batch_size_total_bytes =
+        //         polled_messages.messages[0].get_size_bytes().as_bytes_u64()
+        //             * messages_per_batch as u64;
+        // }
 
-        let metrics = from_records(
-            records,
-            BenchmarkKind::Poll,
-            ActorKind::Consumer,
-            self.consumer_id,
-            self.sampling_time,
-            self.moving_average_window,
-        );
+        //     total_user_data_bytes += IggyByteSize::from(batch_user_size_bytes);
+        //     total_bytes += IggyByteSize::from(batch_size_total_bytes);
+        //     current_iteration += 1;
+        //     let message_batches = current_iteration;
+        //     records.push(BenchmarkRecord {
+        //         elapsed_time_us: start_timestamp.elapsed().as_micros() as u64,
+        //         latency_us: latency.as_micros() as u64,
+        //         messages: received_messages,
+        //         message_batches,
+        //         user_data_bytes: total_user_data_bytes.as_bytes_u64(),
+        //         total_bytes: total_bytes.as_bytes_u64(),
+        //     });
+        // }
 
-        Self::log_consumer_statistics(
-            self.consumer_id,
-            total_messages,
-            message_batches as u32,
-            messages_per_batch,
-            &metrics,
-        );
+        // let metrics = from_records(
+        //     records,
+        //     BenchmarkKind::Poll,
+        //     ActorKind::Consumer,
+        //     self.consumer_id,
+        //     self.sampling_time,
+        //     self.moving_average_window,
+        // );
 
-        Ok(metrics)
-    
+        // Self::log_consumer_statistics(
+        //     self.consumer_id,
+        //     total_messages,
+        //     message_batches as u32,
+        //     messages_per_batch,
+        //     &metrics,
+        // );
+
+        // Ok(metrics)
     }
 
     pub fn log_consumer_statistics(

6a7770cb163ec1f772ef170cbc71efdc76ed9e62
Marvin Hansen <marvin.hansen@gmail.com>
2025-02-12 16:37:18 +0800

    Added some docstring. Fixed code signing by updating gpg to use the correct key.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- sdk/src/consumer_ext/consumer_message_trait.rs
+++ sdk/src/consumer_ext/consumer_message_trait.rs
@@ -5,6 +5,15 @@ use tokio::sync::oneshot;
 
 #[async_trait]
 pub trait IggyConsumerMessageExt {
+    /// This function starts an event loop that consumes messages from the stream and
+    /// applies the provided processor. The loop will exit when the
+    /// shutdown receiver is triggered.
+    ///
+    /// # Arguments
+    ///
+    /// * `event_processor`: The processor to send messages to.
+    /// * `shutdown_rx`: The receiver to listen to for shutdown.
+    ///
     async fn consume_messages(
         mut self,
         event_processor: &'static (impl MessageConsumer + Sync),

013916971b993f4a6c4eebcd2e7de1c48667f92a
numinex <g.koszyk98@gmail.com>
2025-02-22 11:55:57 +0100

    SDK prepre

--- sdk/src/binary/messages.rs
+++ sdk/src/binary/messages.rs
@@ -9,7 +9,7 @@ use crate::messages::flush_unsaved_buffer::FlushUnsavedBuffer;
 use crate::messages::poll_messages::PollingStrategy;
 use crate::messages::send_messages::{Message, Partitioning};
 use crate::messages::{poll_messages, send_messages};
-use crate::models::messages::PolledMessages;
+use crate::models::batch::IggyBatch;
 
 #[async_trait::async_trait]
 impl<B: BinaryClient> MessageClient for B {
@@ -22,7 +22,7 @@ impl<B: BinaryClient> MessageClient for B {
         strategy: &PollingStrategy,
         count: u32,
         auto_commit: bool,
-    ) -> Result<PolledMessages, IggyError> {
+    ) -> Result<IggyBatch, IggyError> {
         fail_if_not_authenticated(self).await?;
         let response = self
             .send_raw_with_response(
@@ -38,7 +38,8 @@ impl<B: BinaryClient> MessageClient for B {
                 ),
             )
             .await?;
-        mapper::map_polled_messages(response)
+        //TODO: Fix me
+        todo!()
     }
 
     async fn send_messages(

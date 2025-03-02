77510a7685105efbcf6bc2e70d9c9f355aa1d5e6
numinex <g.koszyk98@gmail.com>
2025-03-02 20:52:27 +0100

    remove junk from bench

--- bench/src/actors/producing_consumer.rs
+++ bench/src/actors/producing_consumer.rs
@@ -110,7 +110,6 @@ impl ProducingConsumer {
             messages.push(message);
         }
 
-        let xd = Partitioning::messages_key_str("test").unwrap();
         let stream_id = self.stream_id.try_into()?;
         let topic_id = topic_id.try_into()?;
         let partitioning = match self.partitions_count {
